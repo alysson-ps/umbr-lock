0c6bc70b58c8031b0e32482fac841cc5d95529e7
Alisson Porto <dev.alysson@gmail.com>
2025-10-02 00:54:06 -0300

    refactor(parser): rework to support new parenthesis-based syntax inspired by yuck

--- crates/uheex/src/parser.rs
+++ crates/uheex/src/parser.rs
@@ -1,117 +1,340 @@
 use ariadne::{Color, Label, Report, ReportKind, Source};
 use std::collections::BTreeMap;
 use std::ops::Range;
+use std::time::Duration;
 
 use chumsky::error::Rich;
 use chumsky::input::{Stream, ValueInput};
-use chumsky::prelude::{just, recursive};
+use chumsky::prelude::{choice, end, just, recursive};
 use chumsky::span::SimpleSpan;
 use chumsky::{IterParser, Parser, extra, select};
 
-use super::types::{SExpr, Token, Value};
+use crate::types::{
+    BinaryOperator, Declaration, DurationHuman, Expr, Rule, Selector, Stylesheet, Uheex, VNode,
+};
+
+use super::types::{Token, Value};
 
 use logos::Logos;
 
 fn parse<'tokens, 'src: 'tokens, I>()
--> impl Parser<'tokens, I, SExpr, extra::Err<Rich<'tokens, Token<'src>>>>
+-> impl Parser<'tokens, I, Uheex, extra::Err<Rich<'tokens, Token<'src>>>>
 where
     I: ValueInput<'tokens, Token = Token<'src>, Span = SimpleSpan>,
 {
+    let comments = just(Token::Comment).repeated().ignored();
+
+    // let safe_block = nested_delimiters(Token::LParenthesis, Token::RParenthesis, [], |_span| {
+    //     VNode::Error(ErrorKind::Unknown)
+    // });
+
+    let declare = declare().padded_by(comments.clone()).or_not();
+    let widgets = widgets().padded_by(comments.clone());
+    let stylesheet = stylesheet().padded_by(comments.clone()).or_not();
+
+    declare
+        .then(widgets)
+        .then(stylesheet)
+        .padded_by(comments.clone())
+        .then_ignore(end())
+        .map(|((declare, root), stylesheet)| Uheex {
+            globals: declare.unwrap_or(vec![]),
+            root,
+            stylesheet,
+        })
+}
+
+fn widgets<'tokens, 'src: 'tokens, I>()
+-> impl Parser<'tokens, I, VNode, extra::Err<Rich<'tokens, Token<'src>>>> + Clone
+where
+    I: ValueInput<'tokens, Token = Token<'src>, Span = SimpleSpan>,
+{
+    let comments = just(Token::Comment).repeated().ignored();
+
     recursive(|expr| {
         let ident = select! { Token::Identifier(v) => v.to_string() };
 
-        let tag_name = ident
-            .ignore_then(just(Token::Dot))
+        let string =
+            select! { Token::LiteralString(v) => VNode::String(v.trim_matches('\'').to_string()) };
+        let number = select! { Token::Number(v) => VNode::Number(v.parse().unwrap_or(0.0)) };
+        let binding = just(Token::Ampersand)
             .ignore_then(ident)
-            .labelled("Tag name");
+            .map(|v| VNode::Binding(v));
 
-        let end_close = just(Token::Slash)
-            .then(just(Token::TagEnd))
-            .labelled("Self close");
-        let start_close = just(Token::TagStart).then(just(Token::Slash));
+        // FIXME: the span is only hanging on the name of the 'then'
+        // the correct thing would be in the complete block
+        let then = just(Token::Then)
+            .ignore_then(attributes())
+            .then(expr.clone())
+            .try_map(|(attrs, child), span| {
+                if attrs.len() < 1 {
+                    return Err(Rich::custom(span, "then must have a condition"));
+                }
 
-        let attr_name = just(Token::Colon).ignore_then(ident).labelled("Attr name");
-
-        let attr_value = select! {
-            Token::LiteralString(v) => Value::String(v.replace("\"", "").to_string()),
-            Token::Binding(v) => Value::Binding(v.to_string()),
-            Token::Number(v) => Value::Number(v.parse().unwrap())
-        }
-        .labelled("Attr value");
-
-        let attr = attr_name
-            // .then_ignore(just(Token::Equals))
-            .then(attr_value)
-            .map(|(name, value)| (name, value));
-
-        let tag_self_closed = just(Token::TagStart)
-            .ignore_then(tag_name.clone())
-            .then(
-                attr.clone()
-                    .repeated()
-                    .collect::<Vec<(String, Value)>>()
-                    .map(|pairs| pairs.into_iter().collect::<BTreeMap<_, _>>())
-                    .or_not(),
-            )
-            .then_ignore(end_close)
-            .map(|(name, attrs)| SExpr::Element {
-                name: name,
-                attributes: attrs.unwrap_or_default(),
-                children: vec![],
-                self_closing: true,
+                if let Some(cond) = attrs.get("cond") {
+                    Ok(VNode::Then {
+                        cond: Box::new(cond.clone()),
+                        child: Box::new(child),
+                    })
+                } else {
+                    Err(Rich::custom(span, "then must have a condition"))
+                }
             })
-            .labelled("Element self closed");
+            .labelled("then directive");
 
-        let tag_nornal = just(Token::TagStart)
-            .ignore_then(tag_name.clone())
-            .then(
-                attr.clone()
-                    .repeated()
-                    .collect::<Vec<(String, Value)>>()
-                    .map(|pairs| pairs.into_iter().collect::<BTreeMap<_, _>>())
-                    .or_not(),
-            )
-            .then_ignore(just(Token::TagEnd))
-            .then(expr.repeated().collect::<Vec<_>>())
-            .then_ignore(start_close.then(tag_name.clone()).then(just(Token::TagEnd)))
-            // .inspect(|i| println!("{}", i))
-            .map(|((name, attrs), chd)| SExpr::Element {
-                name: name,
-                attributes: attrs.unwrap_or_default(),
-                children: chd,
-                self_closing: false,
+        let repeat = just(Token::Repeat)
+            .ignore_then(attributes())
+            .then(expr.clone())
+            .try_map(|(attrs, child), span| {
+                if attrs.len() < 1 {
+                    return Err(Rich::custom(span, "repeat must have a times attribute"));
+                }
+                if let Some(times) = attrs.get("times") {
+                    Ok(VNode::Repeat {
+                        times: Box::new(times.clone()),
+                        child: Box::new(child),
+                    })
+                } else {
+                    Err(Rich::custom(span, "repeat must have a times attribute"))
+                }
             })
-            .labelled("Element With Children");
+            .labelled("repeat directive");
 
-        let block = select! {Token::BlockBindingValue(v) => v.replace("@", "").to_string()}
-            .map(|bind| SExpr::Binding(bind))
-            .delimited_by(
-                just(Token::BlockBindingOpen),
-                just(Token::BlockBindingClose),
-            );
+        let widget = ident
+            .then(attributes())
+            .then(expr.clone().repeated().at_least(1).collect::<Vec<_>>())
+            .map(|((kind, attrs), child)| VNode::Widget {
+                kind: kind.to_string(),
+                attributes: attrs,
+                child: Box::new(child),
+            });
 
-        tag_self_closed.or(tag_nornal).or(block)
+        let window = just(Token::Window)
+            .ignore_then(attributes())
+            .then(expr.clone().repeated().at_least(1).collect::<Vec<_>>())
+            .map(|(attrs, child)| VNode::Window {
+                attributes: attrs,
+                child: Box::new(child),
+            })
+            .labelled("window directive");
+
+        just(Token::LParenthesis)
+            .ignore_then(
+                window
+                    .or(then)
+                    .or(repeat)
+                    .or(widget)
+                    .or(string)
+                    .or(number)
+                    .or(binding)
+                    .labelled("widget"),
+            )
+            .then_ignore(just(Token::RParenthesis))
+            .padded_by(comments.clone())
     })
 }
 
-pub fn parser(code: &str) -> Option<SExpr> {
+fn stylesheet<'tokens, 'src: 'tokens, I>()
+-> impl Parser<'tokens, I, Stylesheet, extra::Err<Rich<'tokens, Token<'src>>>> + Clone
+where
+    I: ValueInput<'tokens, Token = Token<'src>, Span = SimpleSpan>,
+{
+    let comments = just(Token::Comment).repeated();
+
+    let ident = select! { Token::Identifier(v) => v.to_string() };
+
+    let declarations = recursive(|declarations| {
+        let simple = just(Token::Colon)
+            .ignore_then(ident)
+            .then(expression())
+            .map(|(k, v)| Declaration::Simple {
+                property: k,
+                value: v,
+            })
+            .labelled("simple declaration");
+
+        let nested = ident
+            .then(declarations.clone().repeated().collect::<Vec<_>>())
+            .map(|(k, v)| Declaration::Nested {
+                property: k,
+                value: v,
+            })
+            .delimited_by(just(Token::LParenthesis), just(Token::RParenthesis))
+            .labelled("nested declaration");
+
+        choice((simple, nested))
+    })
+    .padded_by(comments.clone())
+    .repeated()
+    .collect::<Vec<_>>();
+
+    let widget = ident
+        .then(declarations.clone())
+        .map(|(selector, declarations)| Rule {
+            selector: Selector::Tag(selector),
+            declarations,
+        })
+        .delimited_by(just(Token::LParenthesis), just(Token::RParenthesis))
+        .labelled("widget rule");
+
+    let class = just(Token::Dot)
+        .ignore_then(ident)
+        .then(declarations.clone())
+        .map(|(selector, declarations)| Rule {
+            selector: Selector::Class(selector),
+            declarations,
+        })
+        .delimited_by(just(Token::LParenthesis), just(Token::RParenthesis))
+        .labelled("class rule");
+
+    let selector = choice((widget, class))
+        .repeated()
+        .collect::<Vec<_>>()
+        .padded_by(comments.clone());
+
+    just(Token::Stylesheet)
+        .ignore_then(selector)
+        .map(|rules| Stylesheet { rules })
+        .delimited_by(just(Token::LParenthesis), just(Token::RParenthesis))
+}
+
+fn declare<'tokens, 'src: 'tokens, I>()
+-> impl Parser<'tokens, I, Vec<VNode>, extra::Err<Rich<'tokens, Token<'src>>>> + Clone
+where
+    I: ValueInput<'tokens, Token = Token<'src>, Span = SimpleSpan>,
+{
+    let values = select! {
+        Token::LiteralString(v) => Expr::Value(Value::String(v.trim_matches('\'').to_string())),
+        Token::Number(v) => Expr::Value(Value::Number(v.parse().unwrap_or(0.0))),
+        Token::Identifier(v) => Expr::Value(Value::String(v.to_string())),
+    }
+    .delimited_by(just(Token::LParenthesis), just(Token::RParenthesis));
+
+    // TODO: improve shell parsing, now it only supports `$(...)`
+    let shell = just(Token::Dollar)
+        .ignore_then(values.clone())
+        .try_map(|v, span| match v {
+            Expr::Value(Value::String(s)) => Ok(Expr::Shell(s)),
+            _ => Err(Rich::custom(span, "shell must be a string")),
+        });
+
+    let variable = just(Token::Variable)
+        .ignore_then(attributes())
+        .then(shell.or(values))
+        .try_map(|(attrs, value), span| {
+            let name = match attrs.get("name") {
+                Some(Expr::Value(Value::String(s))) => s.clone(),
+                _ => return Err(Rich::custom(span, "variable must have a name attribute")),
+            };
+
+            let initial = match attrs.get("initial") {
+                Some(v) => Some(v.clone()),
+                None => None,
+            };
+
+            let interval = match attrs.get("interval") {
+                Some(Expr::Value(Value::String(s))) => {
+                    s.parse::<DurationHuman>().ok().map(|d| d.0).unwrap()
+                }
+                _ => Duration::from_secs(1),
+            };
+
+            Ok(VNode::Variable {
+                name,
+                initial,
+                value,
+                interval,
+            })
+        })
+        .delimited_by(just(Token::LParenthesis), just(Token::RParenthesis));
+
+    just(Token::Declare)
+        .ignore_then(variable.repeated().collect::<Vec<_>>())
+        .delimited_by(just(Token::LParenthesis), just(Token::RParenthesis))
+}
+
+fn attributes<'tokens, 'src: 'tokens, I>()
+-> impl Parser<'tokens, I, BTreeMap<String, Expr>, extra::Err<Rich<'tokens, Token<'src>>>> + Clone
+where
+    I: ValueInput<'tokens, Token = Token<'src>, Span = SimpleSpan>,
+{
+    let comments = just(Token::Comment).repeated().ignored();
+
+    let ident = select! { Token::Identifier(v) => v.to_string() };
+
+    just(Token::Colon)
+        .ignore_then(ident)
+        .then(expression())
+        .padded_by(comments.clone())
+        .map(|(k, v)| (k, v))
+        .repeated()
+        .collect::<BTreeMap<_, _>>()
+}
+
+fn expression<'tokens, 'src: 'tokens, I>()
+-> impl Parser<'tokens, I, Expr, extra::Err<Rich<'tokens, Token<'src>>>> + Clone
+where
+    I: ValueInput<'tokens, Token = Token<'src>, Span = SimpleSpan>,
+{
+    let comments = just(Token::Comment).repeated().ignored();
+
+    let ident = select! { Token::Identifier(v) => v.to_string() };
+
+    let values = select! {
+        Token::LiteralString(v) => Expr::Value(Value::String(v.trim_matches('\'').to_string())),
+        Token::Number(v) => Expr::Value(Value::Number(v.parse().unwrap_or(0.0))),
+        Token::Identifier(v) => Expr::Value(Value::String(v.to_string())),
+    };
+
+    let binding = just(Token::Ampersand)
+        .ignore_then(ident)
+        .map(|v| Expr::Binding(v));
+
+    let op = select! {
+        Token::Gt => BinaryOperator::Gt,
+        Token::Lt => BinaryOperator::Lt,
+        Token::Plus => BinaryOperator::Add,
+        Token::Minus => BinaryOperator::Sub,
+        Token::Mul => BinaryOperator::Mul,
+        Token::Div => BinaryOperator::Div,
+        Token::Eq => BinaryOperator::Eq,
+        Token::NotEq => BinaryOperator::NotEq,
+        Token::And => BinaryOperator::And,
+        Token::Or => BinaryOperator::Or,
+    };
+
+    let atom = values.or(binding.clone());
+
+    let expr = atom
+        .clone()
+        .foldl_with(op.then(atom).repeated(), |left, (operator, right), _e| {
+            Expr::Binary {
+                kind: "".to_string(),
+                left: Box::new(left),
+                operator,
+                right: Box::new(right),
+            }
+        })
+        .delimited_by(just(Token::LParenthesis), just(Token::RParenthesis));
+
+    expr.or(values).or(binding).padded_by(comments)
+}
+
+pub fn parser(code: &str) {
     let tokens_iter = Token::lexer(code).spanned().map(|(t, s)| match t {
         Ok(t) => (t, <Range<usize> as Into<SimpleSpan>>::into(s)),
         Err(()) => (Token::Error, s.into()),
     });
 
-    // dbg!(tokens_iter.collect::<Vec<_>>());
     use chumsky::input::Input;
     let token_stream = Stream::from_iter(tokens_iter).map((0..code.len()).into(), |(t, s)| (t, s));
-
-    // let (result, err) = parse().parse(token_stream).into_output_errors();
 
     let (result, errs) = parse().parse(token_stream).into_output_errors();
 
     for err in errs {
         Report::build(ReportKind::Error, ("config.uheex", err.span().into_range()))
             .with_config(ariadne::Config::new().with_index_type(ariadne::IndexType::Byte))
-            .with_code(3)
+            .with_code(1)
             .with_message(err.to_string())
             .with_label(
                 Label::new(("config.uheex", err.span().into_range()))
@@ -122,6 +345,4 @@ pub fn parser(code: &str) -> Option<SExpr> {
             .print(("config.uheex", Source::from(code)))
             .unwrap();
     }
-
-    result
 }
--- crates/uheex/src/types.rs
+++ crates/uheex/src/types.rs
@@ -1,46 +1,87 @@
 use core::fmt;
-use std::collections::BTreeMap;
+use std::str::FromStr;
 use logos::Logos;
+use std::collections::BTreeMap;
+use std::time::Duration;
 
 #[derive(Logos, Debug, PartialEq, Clone)]
 pub enum Token<'a> {
-    // === Tag delimiters ===
-    #[token("<%")]
-    BlockBindingOpen,
+    #[token("window")]
+    Window,
 
-    #[token("%>")]
-    BlockBindingClose,
+    #[token("variable")]
+    Variable,
 
-    #[regex(r"@[\w\-]+", priority = 2)]
-    BlockBindingValue(&'a str),
+    #[token("declare")]
+    Declare,
 
-    #[token("<")]
-    TagStart,
+    #[token("stylesheet")]
+    Stylesheet,
 
-    #[token("/")]
-    Slash,
+    #[token("then")]
+    Then,
 
-    #[token(">")]
-    TagEnd,
+    #[token("repeat")]
+    Repeat,
 
-    #[token(".")]
-    Dot,
+    #[token("(")]
+    LParenthesis,
+
+    #[token(")")]
+    RParenthesis,
 
     #[token(":")]
     Colon,
 
+    #[regex(r";;[^\n]*")]
+    Comment,
+
+    #[token(".")]
+    Dot,
+
+    #[token("&")]
+    Ampersand,
+
+    #[token("$")]
+    Dollar,
+
+    // === Operators ===
     #[token("-")]
-    Hifen,
+    Minus,
+
+    #[token("+")]
+    Plus,
+
+    #[token("*")]
+    Mul,
+
+    #[token("/")]
+    Div,
+
+    #[token("==")]
+    Eq,
+
+    #[token("!=")]
+    NotEq,
+
+    #[token("&&")]
+    And,
+
+    #[token("||")]
+    Or,
+
+    #[token(">")]
+    Gt,
+
+    #[token("<")]
+    Lt,
 
     // === Attribute related ===
     #[regex(r"[0-9]+")]
     Number(&'a str),
 
-    #[regex(r#""[^"]*""#)]
+    #[regex(r#"'[^']*'"#)]
     LiteralString(&'a str),
-
-    #[regex(r"\{@[A-Za-z0-9_]+\}")]
-    Binding(&'a str),
 
     // === Misc ===
     #[regex(r"[a-zA-Z_][a-zA-Z0-9_]*")]
@@ -55,18 +96,31 @@ pub enum Token<'a> {
 impl fmt::Display for Token<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::BlockBindingOpen => write!(f, "<%"),
-            Self::BlockBindingClose => write!(f, "%>"),
-            Self::BlockBindingValue(v) => write!(f, "{}", v),
-            Self::Slash => write!(f, "/"),
-            Self::Colon => write!(f, ";"),
-            Self::TagEnd => write!(f, ">"),
-            Self::TagStart => write!(f, "<"),
+            Self::Window => write!(f, "window"),
+            Self::Variable => write!(f, "variable"),
+            Self::Declare => write!(f, "declare"),
+            Self::Stylesheet => write!(f, "stylesheet"),
+            Self::Then => write!(f, "then"),
+            Self::Repeat => write!(f, "repeat"),
+            Self::Ampersand => write!(f, "&"),
+            Self::Dollar => write!(f, "$"),
+            Self::LParenthesis => write!(f, "("),
+            Self::RParenthesis => write!(f, ")"),
+            Self::Gt => write!(f, ">"),
+            Self::Lt => write!(f, "<"),
+            Self::Plus => write!(f, "+"),
+            Self::Mul => write!(f, "*"),
+            Self::Div => write!(f, "/"),
+            Self::Eq => write!(f, "=="),
+            Self::NotEq => write!(f, "!="),
+            Self::And => write!(f, "&&"),
+            Self::Or => write!(f, "||"),
+            Self::Comment => write!(f, "<comment>"),
+            Self::Colon => write!(f, ":"),
             Self::Number(v) => write!(f, "{}", v),
             Self::Dot => write!(f, "."),
-            Self::Hifen => write!(f, "-"),
+            Self::Minus => write!(f, "-"),
             Self::LiteralString(v) => write!(f, "{}", v),
-            Self::Binding(v) => write!(f, "{}", v),
             Self::Identifier(v) => write!(f, "{}", v),
             Self::Whitespace => write!(f, "<whitespace>"),
             Self::Error => write!(f, "<error>"),
@@ -75,20 +129,162 @@ impl fmt::Display for Token<'_> {
 }
 
 #[derive(Debug, Clone)]
-pub enum SExpr {
-    Element {
-        name: String,
-        attributes: BTreeMap<String, Value>,
-        children: Vec<SExpr>,
-        self_closing: bool,
+pub struct Uheex {
+    pub globals: Vec<VNode>,
+    pub root: VNode,
+    pub stylesheet: Option<Stylesheet>,
+}
+
+impl Uheex {
+    pub fn new() -> Self {
+        Self {
+            globals: vec![],
+            root: VNode::Error(ErrorKind::Unknown),
+            stylesheet: None,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Stylesheet {
+    pub rules: Vec<Rule>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Rule {
+    pub selector: Selector,
+    pub declarations: Vec<Declaration>,
+}
+
+#[derive(Debug, Clone)]
+pub enum Selector {
+    Tag(String),
+    Class(String),
+    Id(String),
+}
+
+#[derive(Debug, Clone)]
+pub enum Declaration {
+    Simple {
+        property: String,
+        value: Expr,
     },
-    Text(String),
-    Binding(String), // <% @user %>
+    Nested {
+        property: String,
+        value: Vec<Declaration>,
+    },
+}
+
+#[derive(Debug, Clone)]
+pub enum VNode {
+    Window {
+        attributes: BTreeMap<String, Expr>,
+        child: Box<Vec<VNode>>,
+    },
+    Then {
+        cond: Box<Expr>,
+        child: Box<VNode>,
+    },
+    Repeat {
+        times: Box<Expr>,
+        child: Box<VNode>,
+    },
+    Widget {
+        kind: String,
+        attributes: BTreeMap<String, Expr>,
+        child: Box<Vec<VNode>>,
+    },
+    Variable {
+        name: String,
+        initial: Option<Expr>,
+        value: Expr,
+        interval: Duration,
+    },
+    Declare {
+        vars: Vec<Self>,
+    },
+    String(String),
+    Number(f64),
+    Binding(String),
+
+    Error(ErrorKind),
+}
+
+#[derive(Debug, Clone)]
+pub enum ErrorKind {
+    Unknown,
+}
+
+#[derive(Debug, Clone)]
+pub enum Expr {
+    Value(Value),
+    Binary {
+        kind: String,
+        left: Box<Expr>,
+        operator: BinaryOperator,
+        right: Box<Expr>,
+    },
+    Binding(String),
+    Shell(String),
+}
+
+#[derive(Debug, Clone)]
+pub enum BinaryOperator {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Eq,
+    NotEq,
+    And,
+    Or,
+    Gt,
+    Lt,
+    Gte,
+    Lte,
 }
 
 #[derive(Debug, Clone)]
 pub enum Value {
     String(String),
-    Number(f64), 
+    Number(f64),
     Binding(String),
+}
+
+pub struct DurationHuman(pub Duration);
+
+impl FromStr for DurationHuman {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let s = s.trim();
+        if s.ends_with("ms") {
+            let num = &s[..s.len() - 2];
+            match num.parse::<u64>() {
+                Ok(v) => Ok(DurationHuman(Duration::from_millis(v))),
+                Err(_) => Err(format!("Invalid duration: {}", s)),
+            }
+        } else if s.ends_with('s') {
+            let num = &s[..s.len() - 1];
+            match num.parse::<u64>() {
+                Ok(v) => Ok(DurationHuman(Duration::from_secs(v))),
+                Err(_) => Err(format!("Invalid duration: {}", s)),
+            }
+        } else if s.ends_with('m') {
+            let num = &s[..s.len() - 1];
+            match num.parse::<u64>() {
+                Ok(v) => Ok(DurationHuman(Duration::from_secs(v * 60))),
+                Err(_) => Err(format!("Invalid duration: {}", s)),
+            }
+        } else if s.ends_with('h') {
+            let num = &s[..s.len() - 1];
+            match num.parse::<u64>() {
+                Ok(v) => Ok(DurationHuman(Duration::from_secs(v * 3600))),
+                Err(_) => Err(format!("Invalid duration: {}", s)),
+            }
+        } else {
+            Err(format!("Invalid duration: {}", s))
+        }
+    }
+    
 }
